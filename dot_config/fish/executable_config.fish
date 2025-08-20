b7950559eacdf1947bc6d061cb82caa87193b687
spicydogwings <spaceprotondev@proton.me>
2025-08-19 21:38:42 -0600

    feat(fish): nueva config de fish

---
+++ dot_config/fish/executable_config.fish
@@ -0,0 +1,7 @@
+starship init fish | source
+alias pamcan pacman
+alias ls 'eza --icons'
+alias clear "printf '\033[2J\033[3J\033[1;1H'"
+alias q 'qs -c ii'
+# fnm
+set -gx PATH "$HOME/.local/share/fnm" $PATH
